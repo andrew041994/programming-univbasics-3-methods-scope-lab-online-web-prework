e515459c7ca1dbcc86c079235b41d63b7148d973
Alehandro Persaud <andrew041994@gmail.com>
2019-12-22 01:39:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,9 @@
-
-$  status = "Thank You Mario! But Our Princess Is In Another Castle!"
-def mario
-  phrase = "It's-a me, Mario!"
-  puts phrase
+def link
+  puts "It's Dangerous To Go Alone! Take This."
 end
 
-def toadstool
-  puts status
+  def any_phrase do
+    phrase = "Do A Barrel Roll!"
+    expect{any_phrase(phrase)} .to output(Do A Barrel Roll!\n) .to_stdout
+  end
 end
